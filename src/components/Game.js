5fa7e5e18eca56460616ac711f7a68a7ad40c175
Konrad <konradbieniek@gmail.com>
2022-11-22 14:02:48 +0100

    added Card comp

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,15 @@
 import styles from "./Game.module.css"
+import Card from "./Card.js"
 
 const Game = (props) => {
 	return (
-		<div className={styles.game}>
+		<Card className={styles.game}>
 			<div>{props.title}</div>
 			<div className={styles["game-packing"]}>
 				<h2>Pakowane: {props.package}</h2>
 				<div className={styles["game-price"]}>SCD: {props.scd}</div>
 			</div>
-		</div>
+		</Card>
 	)
 }
 
