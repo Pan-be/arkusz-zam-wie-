40fc4971434d739f9433343aec2303f45886ce00
Konrad <konradbieniek@gmail.com>
2022-11-23 23:40:37 +0100

    styled input

--- src/components/OrderInput.js
+++ src/components/OrderInput.js
@@ -1,10 +1,37 @@
+import { useState } from "react"
 import styles from "./OrderInput.module.css"
 
 const OrderInput = (props) => {
+	const [num, setNum] = useState("")
+	const addition = () => {
+		console.log("added")
+		setNum(parseInt(num + 1))
+	}
+
+	const substraction = () => {
+		if (num > 0) {
+			setNum(parseInt(num - 1))
+		}
+	}
+	const handleChange = (e) => {
+		setNum(parseInt(e.target.value))
+	}
 	return (
 		<div className={styles.input}>
-			<label for='order'>Zamawiana ilość</label>
-			<input type='number' name='order' />
+			<label htmlFor='order'>Zamawiana ilość</label>
+			<div className={styles["input-container"]}>
+				<span className={styles.prev} onClick={substraction}></span>
+				<span className={styles.next} onClick={addition}></span>
+				<div>
+					<input
+						type='number'
+						name='order'
+						min='0'
+						value={num}
+						onChange={handleChange}
+					/>
+				</div>
+			</div>
 		</div>
 	)
 }
